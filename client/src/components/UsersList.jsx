dd0add70e4b18295ff582500fb71d235b498c2d0
DestinyOwl <khaldi.achraf2001@gmail.com>
2023-08-02 18:35:36 +0100

    Updated chat client

--- client/src/components/UsersList.jsx
+++ client/src/components/UsersList.jsx
@@ -7,12 +7,19 @@ function UsersList() {
   const listItems = list.map((user) => (
     <div
       key={user._id}
-      className="w-full border-b-4 py-3 text-base-200 font-rubik-regular cursor-pointer text-center hover:bg-primary hover:text-white"
+      className="w-full border-b-4 py-3 shadow-xl bg-white text-base-200 font-rubik-regular cursor-pointer text-center hover:bg-primary hover:text-white"
     >
       {user.username}
     </div>
   ));
-  return <div className="h-full shadow-xl w-1/5">{listItems}</div>;
+  return (
+    <div className="h-full w-1/5 bg-white">
+      <div className="p-6 text-center font-rubik-medium bg-primary text-white border-b-4 ">
+        Users
+      </div>
+      <div>{listItems}</div>
+    </div>
+  );
 }
 
 export default UsersList;
--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -3,7 +3,7 @@ import { logged } from "../context/LogContext";
 import axios from "axios";
 import { users } from "../context/UsersContext";
 import UsersList from "../components/UsersList";
-
+import logo from "./assets/display.png";
 function Chat() {
   const data = useContext(logged);
   const [user, setUser] = useState();
@@ -22,18 +22,25 @@ function Chat() {
   }, [data]);
   return (
     <div className="h-screen w-full bg-white">
-      <div className="navbar bg-white shadow-xl">
+      <div className="navbar bg-info shadow-xl">
         <div className="flex-1">
-          <span className="text-base-200 mx-3 font-rubik-regular">
-            Welcome,{" "}
-            <span className="hover:text-primary">
-              {user ? user.firstname : null}
-            </span>
+          <span className="flex mx-3 font-rubik-medium font-bold text-white">
+            <div className="bg-white rounded-xl w-8 h-8 p-1 mr-3 shadow-xl">
+              <img src={logo} />
+            </div>
+            <div className="grid content-center">
+              <div>
+                Welcome,{" "}
+                <span className="hover:text-primary">
+                  {user ? user.firstname : null}
+                </span>
+              </div>
+            </div>
           </span>
         </div>
         <div className="flex-none">
           <button
-            className="justify-self-end btn btn-error font-rubik-medium text-white mx-4"
+            className="justify-self-end btn btn-primary font-rubik-medium text-white mx-4"
             onClick={() => {
               logout();
             }}
